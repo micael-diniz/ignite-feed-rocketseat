13e45e97b3633a75166e39bff0a8daed91a634b7
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-06 08:26:55 -0300

    feat(Getting started with React): Properties
    Passing properties to Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import { Post } from "./Post";
 export function App() {
 	return (
 		<Fragment>
-			<Post />
-			<Post />
-			<Post />
-			<Post />
-			<Post />
+			<Post
+				author="Micael Diniz"
+				content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus molestiae voluptas hic quidem nobis quas delectus earum consectetur aut. Cupiditate quo in, natus expedita nesciunt fugit illo repellat assumenda asperiores!"
+			/>
+      <Post
+				author="John Doe"
+				content="Hello world"
+			/>
 		</Fragment>
 	);
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,8 @@
-export function Post() {
-	return <p>Post</p>;
+export function Post(props) {
+	return (
+    <div>
+      <h2>{props.author}</h2>
+      <p>{props.content}</p>
+    </div>
+  );
 }
