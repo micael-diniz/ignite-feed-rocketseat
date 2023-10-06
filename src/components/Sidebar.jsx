15a509e2f236bc226e1d4dcc88f6248363fc49b0
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-06 13:12:48 -0300

    feat(Application Structure): Finishing Sidebar
    add profile image

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,29 @@
-import styles from './Sidebar.module.css';
+import { PencilLine } from "phosphor-react";
+import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
-  return (
-    <aside className={styles.sidebar}>
-      <img
-        className={styles.cover}
-        src="https://images8.alphacoders.com/679/679478.jpg"
-      />
+	return (
+		<aside className={styles.sidebar}>
+			<img
+				className={styles.cover}
+				src="https://images8.alphacoders.com/679/679478.jpg"
+			/>
 
-      <div className={styles.profile}>
-        <strong>Micael Diniz</strong>
-        <span>Full Stack Developer</span>
-      </div>
+			<div className={styles.profile}>
+				<img
+					className={styles.avatar}
+					src="https://github.com/micael-diniz.png"
+				/>
+				<strong>Micael Diniz</strong>
+				<span>Full Stack Developer</span>
+			</div>
 
-      <footer>
-        <a href="#">
-          Editar seu perfil
-        </a>
-      </footer>
-    </aside>
-  );
+			<footer>
+				<a href="#">
+					<PencilLine size={20} />
+					Editar seu perfil
+				</a>
+			</footer>
+		</aside>
+	);
 }
