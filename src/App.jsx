4bdc3c3142f15db997c511e241d1bad5b714f3b5
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-06 13:02:13 -0300

    feat(Application Structure): Component - Post
    add HTML structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Fragment } from "react";
-import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 import { Header } from "./components/Header";
 
 import styles from "./App.module.css";
 import "./global.css";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
 	return (
