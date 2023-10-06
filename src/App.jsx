60c0f4eb11f56849f9c0b88dc34bb1099cd1e3f0
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-06 08:40:59 -0300

    feat(Application Structure): CSS Modules
    import css as module and create scoped styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Fragment } from "react";
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./styles.css";
 
 export function App() {
 	return (
 		<Fragment>
+			<Header />
 			<Post
 				author="Micael Diniz"
 				content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus molestiae voluptas hic quidem nobis quas delectus earum consectetur aut. Cupiditate quo in, natus expedita nesciunt fugit illo repellat assumenda asperiores!"
 			/>
-      <Post
-				author="John Doe"
-				content="Hello world"
-			/>
+			<Post author="John Doe" content="Hello world" />
 		</Fragment>
 	);
 }
