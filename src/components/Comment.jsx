5a63fd62823a322effd16e66b037da2bbf7ea86e
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-07 11:12:23 -0300

    feat(Application Structure): Comment styles

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -11,12 +11,12 @@ export function Comment() {
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Diego Fernandes</strong>
+              <strong>Micael Diniz</strong>
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
 
