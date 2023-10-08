91ab1d67a6c4b762392837f754803de995d29271
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-08 12:56:12 -0300

    feat(React engine): Validating comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -36,12 +36,17 @@ export function Post({ author, publishedAt, content }) {
 		setComments([...comments, newCommentText]);
 
 		setNewCommentText("");
-		
+		console.log("here");
 		removeFocusOfSubmitButton();
 	}
 
 	function handleNewCommentChange() {
+		event.target.setCustomValidity("");
 		setNewCommentText(event.target.value);
+	}
+
+	function handleNewCommentInvalid() {
+		event.target.setCustomValidity("Esse campo é obrigatório!");
 	}
 
 	function deleteComment(commentToDelete) {
@@ -51,6 +56,8 @@ export function Post({ author, publishedAt, content }) {
 
 		setComments(commentsWithoutDeletedOne);
 	}
+
+	const isNewCommentEmpty = newCommentText.length === 0;
 
 	return (
 		<article className={styles.post}>
@@ -92,10 +99,12 @@ export function Post({ author, publishedAt, content }) {
 					placeholder="Deixe um comentário"
 					value={newCommentText}
 					onChange={handleNewCommentChange}
+					onInvalid={handleNewCommentInvalid}
+					required
 				/>
 
 				<footer>
-					<button type="submit" ref={submitButton}>
+					<button type="submit" ref={submitButton} disabled={isNewCommentEmpty}>
 						Publicar
 					</button>
 				</footer>
