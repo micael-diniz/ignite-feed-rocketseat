d3922cb5ffd12e39d2517fc71fc001a6012909be
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-08 11:36:22 -0300

    feat(React engine): State (useState)
    manage comments using the useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 
@@ -7,6 +9,7 @@ import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post({ author, publishedAt, content }) {
+	const [comments, setComments] = useState([1, 2]);
 	const publishedDateFormatted = format(
 		publishedAt,
 		"d 'de' LLLL 'às' HH:mm'h'",
@@ -19,6 +22,12 @@ export function Post({ author, publishedAt, content }) {
 		locale: ptBR,
 		addSuffix: true,
 	});
+
+	function handleCrateNewComment() {
+		event.preventDefault();
+
+		setComments([...comments, comments.length + 1]);
+	}
 	return (
 		<article className={styles.post}>
 			<header>
@@ -51,7 +60,7 @@ export function Post({ author, publishedAt, content }) {
 					}
 				})}
 			</div>
-			<form className={styles.commentForm}>
+			<form onSubmit={handleCrateNewComment} className={styles.commentForm}>
 				<strong>Deixe seu feedback</strong>
 
 				<textarea placeholder="Deixe um comentário" />
@@ -62,9 +71,9 @@ export function Post({ author, publishedAt, content }) {
 			</form>
 
 			<div className={styles.commentList}>
-				<Comment />
-				<Comment />
-				<Comment />
+				{comments.map((comment, index) => {
+					return <Comment key={index} />;
+				})}
 			</div>
 		</article>
 	);
