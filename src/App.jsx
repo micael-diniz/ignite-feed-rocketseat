2576e872dbdcccbb550bfef2609b07083dd1a6ab
Micael Diniz <micaeldiniz6@gmail.com>
2023-10-08 10:44:37 -0300

    feat(React engine): Iterating in JSX
    create posts array and pass information to the Post component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import { Header } from "./components/Header";
 import styles from "./App.module.css";
 import "./global.css";
 
+const posts = [
+	{
+		id: 1,
+		author: {
+			avatarUrl: "https://github.com/micael-diniz.png",
+			name: "Micael Diniz",
+			role: "Web Developer",
+		},
+		content: [
+			{ type: "paragraph", content: "Fala galera 👋" },
+			{
+				type: "paragraph",
+				content:
+					"Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+			},
+			{ type: "link", content: "jane.design/doctorcare" },
+		],
+		publishedAt: new Date("2022-05-03 20:00:00"),
+	},
+	{
+		id: 2,
+		author: {
+			avatarUrl: "https://github.com/micael-diniz.png",
+			name: "Micael Diniz",
+			role: "Web Developer",
+		},
+		content: [
+			{ type: "paragraph", content: "Fala galera 👋" },
+			{
+				type: "paragraph",
+				content:
+					"Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+			},
+			{ type: "link", content: "jane.design/doctorcare" },
+		],
+		publishedAt: new Date("2022-05-10 20:00:00"),
+	},
+];
+
 export function App() {
 	return (
 		<Fragment>
@@ -13,11 +52,16 @@ export function App() {
 			<section className={styles.wrapper}>
 				<Sidebar />
 				<main>
-					<Post
-						author="Micael Diniz"
-						content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus molestiae voluptas hic quidem nobis quas delectus earum consectetur aut. Cupiditate quo in, natus expedita nesciunt fugit illo repellat assumenda asperiores!"
-					/>
-					<Post author="John Doe" content="Hello world" />
+					{posts.map((post) => {
+						return (
+							<Post
+								key={post.id}
+								author={post.author}
+								content={post.content}
+								publishedAt={post.publishedAt}
+							/>
+						);
+					})}
 				</main>
 			</section>
 		</Fragment>
